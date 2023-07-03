9f8a4bdd484ed2ac86a8cb612137ddaa4d99b7e1
haihuilab <zhanghaihui@pku.edu.cn>
2023-07-03 07:39:44 -0700

    “version_1.0.2”

--- R/thepubr.R
+++ R/thepubr.R
@@ -136,13 +136,13 @@ theme_border <- function(base_size = 12,
 #'
 # scale_color_publication
 scale_color_publication <- function(alpha = 1, ...) {
-  values = c("#2F4F4F",  "#660066", "#336600",  "#386cb0", "#A52A2A",
-             "#D2691E", "#7fc97f", "#fb9a99","#B8860B", "#CD1076",
-             "aquamarine4", "#193300", "#0000FF", "#003366", "#660000",
+  values = c( "#386cb0", "#660066","#336600",  "#D2691E",  "#A52A2A",
+              "#FFB90F", "#7fc97f", "#fb9a99","#B8860B", "#CD1076",
+             "aquamarine4",,"#E5CCFF", "#193300", "#0000FF", "#003366",
              "#663300", "#666600", "#7FFF00", "#330066", "#00FFFF",
              "#838B8B", "#330019", "#ffff33",  "#DEB887", "#5F9EA0",
-             "#FFB90F", "#8B4513", "#8B2323", "#FF7F50", "#a6cee3",
-             "#66B2FF", "#8B7D6B", "#FF3319","#E5CCFF",  "#CCCC00")
+             "#2F4F4F", "#8B4513", "#8B2323", "#FF7F50", "#a6cee3",
+             "#66B2FF", "#8B7D6B", "#FF3319",  "#660000", "#CCCC00")
   if (alpha == 1) {
     scale_colour_manual(values = values, ...)
   } else {
