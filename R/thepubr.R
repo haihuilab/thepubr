60e392a6ce6b20bfe37121b0840e2fe9eafb0521
haihuilab <zhanghaihui@pku.edu.cn>
2023-07-03 19:30:35 -0700

    “version_1.0.2”

--- R/thepubr.R
+++ R/thepubr.R
@@ -138,7 +138,7 @@ theme_border <- function(base_size = 12,
 scale_color_publication <- function(alpha = 1, ...) {
   values = c( "#386cb0", "#660066","#336600",  "#D2691E",  "#A52A2A",
               "#FFB90F", "#7fc97f", "#fb9a99","#B8860B", "#CD1076",
-             "aquamarine4","#E5CCFF", "#193300", "#0000FF", "#003366",
+             "aquamarine4","#E5CCFF", "#7071b1", "#0000FF", "#003366",
              "#663300", "#666600", "#7FFF00", "#330066", "#00FFFF",
              "#838B8B", "#330019", "#ffff33",  "#DEB887", "#5F9EA0",
              "#2F4F4F", "#8B4513", "#8B2323", "#FF7F50", "#a6cee3",
