a76677f147044fcda1065771af82855aa9467fcd
haihuilab <zhanghaihui@pku.edu.cn>
2023-08-11 14:40:37 -0700

    “version_1.0.2”

--- R/colors.R
+++ R/colors.R
@@ -25,14 +25,10 @@ pub_colors <- function(alpha = 1,n = 1, ...) {
 #'
 #' @param values a list of color values
 #' @param alpha alpha value (0, 1)
-#' @param aes default is "colour" with scale_colour_manual,
-#' use aesthetic mapping with "colour" like geom_line() and geom_point().
-#' If aes = "fill" with scale_fill_manual,
-#' use aesthetic mapping with "fill" like geom_bar(), geom_area(), and geom_polygon().
 #'
 #' @export
 #'
-pub_scale_colors <- function(alpha = 1, aes = "colour", ...) {
+pub_scale_colors <- function(alpha = 1, ...) {
   colors = c( "#386cb0", "#660066","#336600",  "#D2691E",  "#A52A2A",
               "#FFB90F", "#7fc97f", "#fb9a99","#FF6868", "#B8860B",
               "#CD1076", "aquamarine4", "#7071b1","#003366", "#330066",
@@ -41,16 +37,7 @@ pub_scale_colors <- function(alpha = 1, aes = "colour", ...) {
               "#2F4F4F", "#8B4513", "#8B2323", "#FF7F50", "#a6cee3",
               "#66B2FF", "#8B7D6B", "#FF3319",  "#660000", "#CCCC00")
 
-
-  # aesthetic mapping with `fill` like geom_bar(), geom_area(), and geom_polygon().
-  if (aes == "colour" | aes == "color") {
-  scale_colour_manual(values = alpha(colors, alpha), ...)
-    }
-
-  # aesthetic mapping with `color` like geom_line() and geom_point().
-  if (aes == "fill") {
-    scale_fill_manual(values = alpha(colors, alpha), ...)
-    }
+  scale_discrete_manual(values = alpha(colors, alpha), ...)
 
 }
 
