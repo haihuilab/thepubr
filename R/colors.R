35e497f7a6575625c77d2ecfe0d6360a29e14ab1
haihuilab <zhanghaihui@pku.edu.cn>
2023-08-11 13:32:44 -0700

    “version_1.0.2”

--- R/colors.R
+++ R/colors.R
@@ -2,12 +2,11 @@
 #'
 #' @param values a list of color values
 #' @param alpha alpha value (0, 1)
-#' @param n  could be vector ranging c(1, 35)
+#' @param n  could be vector ranging 1:35
 #'
 #' @export
 #'
-# thepubr_colors
-thepubr_colors <- function(alpha = 1,n = 1, ...) {
+pub_colors <- function(alpha = 1,n = 1, ...) {
   values = c( "#386cb0", "#660066","#336600",  "#D2691E",  "#A52A2A",
               "#FFB90F", "#7fc97f", "#fb9a99","#B8860B", "#CD1076",
               "aquamarine4","#E5CCFF", "#7071b1", "#0000FF", "#003366",
@@ -33,25 +32,24 @@ thepubr_colors <- function(alpha = 1,n = 1, ...) {
 #'
 #' @export
 #'
-# thepubr_scale_colors
-thepubr_scale_colors <- function(alpha = 1, aes = "colour", ...) {
-
-  values = c( "#386cb0", "#660066","#336600",  "#D2691E",  "#A52A2A",
-              "#FFB90F", "#7fc97f", "#fb9a99","#B8860B", "#CD1076",
-              "aquamarine4","#E5CCFF", "#7071b1", "#0000FF", "#003366",
-              "#663300", "#666600", "#7FFF00", "#330066", "#00FFFF",
+pub_scale_colors <- function(alpha = 1, aes = "colour", ...) {
+  colors = c( "#386cb0", "#660066","#336600",  "#D2691E",  "#A52A2A",
+              "#FFB90F", "#7fc97f", "#fb9a99", "#BEADFA","#B8860B",
+              "#CD1076", "aquamarine4", "#7071b1","#003366", "#330066",
+              "#663300", "#666600", "#7FFF00",  "#4E4FEB", "#00FFFF",
               "#838B8B", "#330019", "#ffff33",  "#DEB887", "#5F9EA0",
               "#2F4F4F", "#8B4513", "#8B2323", "#FF7F50", "#a6cee3",
               "#66B2FF", "#8B7D6B", "#FF3319",  "#660000", "#CCCC00")
 
+
   # aesthetic mapping with `fill` like geom_bar(), geom_area(), and geom_polygon().
   if (aes == "colour" | aes == "color") {
-    scale_colour_manual(values = alpha(values, alpha), ...)
-  }
+  scale_colour_manual(values = alpha(colors, alpha), ...)
+    }
 
   # aesthetic mapping with `color` like geom_line() and geom_point().
   if (aes == "fill") {
-    scale_fill_manual(values = alpha(values, alpha), ...)
+    scale_fill_manual(values = alpha(colors, alpha), ...)
     }
 
 }
